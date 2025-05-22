d87672243980028bc9530f661c31fb7d0fb5625e
DimRev <dimarev444@gmail.com>
2025-05-22 18:32:17 +0300

    chore(init): Init nest project

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 42069);
+}
+
+console.log(
+  `Starting API proxy server on http://localhost:${process.env.PORT ?? 42069}`,
+);
+
+void bootstrap();
