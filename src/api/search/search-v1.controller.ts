ac5b1c777cfac5633e6e61a5117b969a10560706
DimRev <dimarev444@gmail.com>
2025-05-23 19:06:38 +0300

    feat(logger): Add duration logs to each route

--- src/api/search/search-v1.controller.ts
+++ src/api/search/search-v1.controller.ts
@@ -20,45 +20,73 @@ export class SearchV1Controller {
   async getSearch(
     @Query('q') query: string,
   ): Promise<SearchResult[] | undefined> {
-    if (!query || query.length < 1) {
-      this.logger.error(
-        'Bad request: Query parameter is required',
+    const startTS = Date.now();
+    try {
+      if (!query || query.length < 1) {
+        this.logger.error(
+          'Bad request: Query parameter is required',
+          'search-v1.controller.getSearch',
+        );
+        throw ApiError.badRequest(
+          'Query parameter is required',
+          'query is required',
+        );
+      }
+
+      const resp = await this.searchV1Service.getSearchResults(query);
+      const endTS = Date.now();
+      this.logger.log(
+        `OK took: ${endTS - startTS}ms`,
         'search-v1.controller.getSearch',
       );
-      throw ApiError.badRequest(
-        'Query parameter is required',
-        'query is required',
+      return resp;
+    } catch (err) {
+      this.logger.error(
+        `FAILED took: ${Date.now() - startTS}ms`,
+        'search-v1.controller.getSearch',
       );
+      throw err;
     }
-
-    const resp = await this.searchV1Service.getSearchResults(query);
-
-    return resp;
   }
 
   @Post()
   async postSearch(@Body() bodyDto: any): Promise<SearchResult[] | undefined> {
-    const bodySchema = z.object({
-      query: z.string().min(1, 'Query parameter is required'),
-    });
+    const startTS = Date.now();
+    try {
+      const bodySchema = z.object({
+        query: z.string().min(1, 'Query parameter is required'),
+      });
 
-    const parsedBody = bodySchema.safeParse(bodyDto);
+      const parsedBody = bodySchema.safeParse(bodyDto);
 
-    if (!parsedBody.success) {
-      const errorMessage = parsedBody.error.errors
-        .map((err) => err.message)
-        .join(', ');
+      if (!parsedBody.success) {
+        const errorMessage = parsedBody.error.errors
+          .map((err) => err.message)
+          .join(', ');
+        this.logger.error(
+          `Bad request: ${errorMessage}`,
+          'search-v1.controller.postSearch',
+        );
+        throw ApiError.badRequest('Invalid request body', errorMessage);
+      }
+
+      const { query } = parsedBody.data;
+
+      const resp = await this.searchV1Service.getSearchResults(query);
+      const endTS = Date.now();
+      this.logger.log(
+        `OK took: ${endTS - startTS}ms`,
+        'search-v1.controller.getSearch',
+      );
+      return resp;
+    } catch (err) {
+      const endTS = Date.now();
       this.logger.error(
-        `Bad request: ${errorMessage}`,
+        `FAILED took: ${endTS - startTS}ms`,
         'search-v1.controller.postSearch',
       );
-      throw ApiError.badRequest('Invalid request body', errorMessage);
+      throw err;
     }
-
-    const { query } = parsedBody.data;
-
-    const resp = await this.searchV1Service.getSearchResults(query);
-    return resp;
   }
 
   @Get('history')
@@ -66,46 +94,61 @@ export class SearchV1Controller {
     @Query('page') pageParam: string,
     @Query('pageSize') pageSizeParam: string,
   ): Promise<PaginatedQueryHistory | undefined> {
-    const queryParamsSchema = z.object({
-      page: z
-        .string()
-        .transform((val) => parseInt(val, 10))
-        .refine((val) => !isNaN(val), {
-          message: 'page must be a valid number',
-        })
-        .refine((val) => val >= 1, {
-          message: 'page must be greater than or equal to 1',
-        }),
-      pageSize: z
-        .string()
-        .transform((val) => parseInt(val, 10))
-        .refine((val) => !isNaN(val), {
-          message: 'page must be a valid number',
-        })
-        .refine((val) => val >= 1, {
-          message: 'page must be greater than or equal to 1',
-        }),
-    });
+    const startTS = Date.now();
+    try {
+      const queryParamsSchema = z.object({
+        page: z
+          .string()
+          .transform((val) => parseInt(val, 10))
+          .refine((val) => !isNaN(val), {
+            message: 'page must be a valid number',
+          })
+          .refine((val) => val >= 1, {
+            message: 'page must be greater than or equal to 1',
+          }),
+        pageSize: z
+          .string()
+          .transform((val) => parseInt(val, 10))
+          .refine((val) => !isNaN(val), {
+            message: 'page must be a valid number',
+          })
+          .refine((val) => val >= 1, {
+            message: 'page must be greater than or equal to 1',
+          }),
+      });
 
-    const parsedQueryParams = queryParamsSchema.safeParse({
-      page: pageParam,
-      pageSize: pageSizeParam,
-    });
+      const parsedQueryParams = queryParamsSchema.safeParse({
+        page: pageParam,
+        pageSize: pageSizeParam,
+      });
 
-    if (!parsedQueryParams.success) {
-      const errorMessage = parsedQueryParams.error.errors
-        .map((err) => err.message)
-        .join(', ');
-      this.logger.error(
-        `Bad request: ${errorMessage}`,
+      if (!parsedQueryParams.success) {
+        const errorMessage = parsedQueryParams.error.errors
+          .map((err) => err.message)
+          .join(', ');
+        this.logger.error(
+          `Bad request: ${errorMessage}`,
+          'search-v1.controller.getHistory',
+        );
+        throw ApiError.badRequest('Invalid request body', errorMessage);
+      }
+
+      const { page, pageSize } = parsedQueryParams.data;
+
+      const resp = await this.searchV1Service.getHistory(page, pageSize);
+      const endTS = Date.now();
+      this.logger.log(
+        `OK took: ${endTS - startTS}ms`,
         'search-v1.controller.getHistory',
       );
-      throw ApiError.badRequest('Invalid request body', errorMessage);
+      return resp;
+    } catch (err) {
+      const endTS = Date.now();
+      this.logger.error(
+        `FAILED took: ${endTS - startTS}ms`,
+        'search-v1.controller.getHistory',
+      );
+      throw err;
     }
-
-    const { page, pageSize } = parsedQueryParams.data;
-
-    const resp = await this.searchV1Service.getHistory(page, pageSize);
-    return resp;
   }
 }
